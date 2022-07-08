5a7a1efee6d4af61536a6033b4c80c83a4ecf178
Albert Avkhadeev <albertavkhadeev@gmail.com>
2022-07-08 23:57:53 +0300

    New commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:cv_io/page/profile_page.dart';
-import 'package:cv_io/page/edit_profile_page.dart';
+import 'package:cv_io/pages/profile_page.dart';
+import 'package:cv_io/pages/edit_profile_page.dart';
 
 void main() => runApp(
       MaterialApp(
